a44d47c123a85aafcf5a66140bc7c1cc02192138
sneha sharma <PrincessSneha@gmail.com>
2020-10-16 19:21:32 +0000

    made changes to loading money to card

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,7 +2,15 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
        // System.out.println("hey");
+
      TerrapinCard card = new TerrapinCard(10);
+     System.out.println(card);
+     card.loadMoney(15);
+     System.out.println(card);
+     card.loadMoney(10);
+     System.out.println(card);
+     card.loadMoney(200);
+     System.out.println(card);
      System.out.println("Billy: " + card);
      card.loadMoney(-15);
      System.out.println("Billy: " + card);
