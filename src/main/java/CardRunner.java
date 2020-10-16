5311c9f6ea15fb8c2cf7613b2ba48b9048a6df76
sneha sharma <PrincessSneha@gmail.com>
2020-10-16 18:55:30 +0000

    added for multiple cards png 6

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,16 +2,21 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
        // System.out.println("hey");
-        TerrapinCard card = new TerrapinCard(10);
-        System.out.println("Billy: " + card);
+     TerrapinCard cardBilly = new TerrapinCard(20);
+     TerrapinCard cardBrian = new TerrapinCard(30);
+     cardBilly.payGourmet();
+     cardBrian.payEconomical();
+     System.out.println("Billy: " + cardBilly);
+     System.out.println("Brian: " + cardBrian);
+     cardBilly.loadMoney(20);
+     cardBrian.payGourmet();
+     System.out.println("Billy: " + cardBilly);
+     System.out.println("Brian: " + cardBrian);
+     cardBilly.payEconomical();
+     cardBilly.payEconomical();
+     cardBrian.loadMoney(50);
+     System.out.println("Billy: " + cardBilly);
+     System.out.println("Brian: " + cardBrian);
 
-        card.loadMoney(-15);
-        System.out.println("Billy: " + card);
-
-       // card.loadMoney(10);
-       // System.out.println(card);
-
-       // card.loadMoney(200);
-       // System.out.println(card);
     }
 }
