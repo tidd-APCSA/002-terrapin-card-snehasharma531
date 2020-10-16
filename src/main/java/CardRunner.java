0ffb3b304123c7295c652753a9698dc158e47a4e
sneha sharma <PrincessSneha@gmail.com>
2020-10-16 15:33:38 +0000

    added balance non negative png3

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,15 +1,14 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        //System.out.println("hey");
-        TerrapinCard card = new TerrapinCard(50);
+       // System.out.println("hey");
+        TerrapinCard card = new TerrapinCard(5);
         System.out.println(card);
 
-        card.payEconomical();
-        System.out.println(card);
-       
         card.payGourmet();
-        card.payEconomical();
+        System.out.println(card);
+
+        card.payGourmet();
         System.out.println(card);
 
     }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -5,10 +5,18 @@ public class TerrapinCard {
     balance = balanceAtStart;
   }
   public void payEconomical(){
+    if(balance >= 2.5){
     balance -= 2.50;
+  }else{
+    balance = balance;
+  }
   }
   public void payGourmet(){
+    if(balance >= 4.0){
     balance -= 4.00;
+  }else{
+    balance = balance;
+  }
   }
   public String toString(){
     return "The card has " + balance + " dollors";
